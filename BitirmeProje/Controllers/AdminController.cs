3466faaef5fdd094d64faa7d76203d54880509da
Onur <iletonurdogan@gmail.com>
2023-09-17 07:34:23 +0300

    Model Oluşturuldu ve Admin sayfası giriş yapıldı.

---
+++ BitirmeProje/Controllers/AdminController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BitirmeProje.Models;
+namespace BitirmeProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        DbWebSiteEntities db = new DbWebSiteEntities();
+        // GET: Admin
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(string kadi,string sifre)
+        {
+            Session["uyem"] = db.UYELER.ToList().Find(x => x.KADI == kadi && x.SIFRE == sifre);
+            if (Session["uyem"] != null)
+            {
+                ViewBag.ekle = "ok";
+                return View("index");
+            }
+            else
+            {
+                ViewBag.ekle = "no";
+                return View();
+            }
+            
+
+        }
+        public ActionResult index()
+        {
+            if (Session["uyem"] != null)
+            { 
+            return View();
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+        public ActionResult BlogListele()
+        {
+            if (Session["uyem"] != null)
+            {
+                var listele = db.BLOGLAR.ToList();
+                return View(listele);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
--- BitirmeProje/Controllers/HomeController.cs
+++ BitirmeProje/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace BitirmeProje.Controllers
         {
             return View();
         }
-
+        
         
     }
 }
